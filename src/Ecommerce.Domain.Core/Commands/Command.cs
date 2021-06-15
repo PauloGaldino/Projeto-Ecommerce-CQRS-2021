e5e5d488fb019ae8df50dfb1338439b171b5957f
JOSE GALDINO <jose.galdino4@fatec.sp.gov.br>
2021-06-15 16:49:37 -0300

    Implementado a classe Command na Camada Domain.Core

---
+++ src/Ecommerce.Domain.Core/Commands/Command.cs
@@ -0,0 +1,23 @@
+﻿using Ecommerce.Domain.Core.Events;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Domain.Core.Commands
+{
+    public abstract class Command : Event
+    {
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+
+        public DateTime Timestamp { get; private set; }
+        public ValidationResult ValidationResult { get; private set; }
+
+        public abstract bool IsValid();
+    }
+}
---
+++ src/Ecommerce.Domain.Core/Notifications/DomainNotification.cs
@@ -0,0 +1,25 @@
+﻿using Ecommerce.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Domain.Core.Notifications
+{
+    public class DomainNotification : Event
+    {
+        public DomainNotification(string key, string value)
+        {
+            DomainNotificationId = Guid.NewGuid();
+            Version = 1;
+            Key = key;
+            Value = value;
+        }
+
+        public Guid DomainNotificationId { get; private set; }
+        public string Key { get; private set; }
+        public string Value { get; private set; }
+        public int Version { get; private set; }
+    }
+}
