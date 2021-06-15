69d5c207a957125a11a997550c91ddca875d70ae
JOSE GALDINO <jose.galdino4@fatec.sp.gov.br>
2021-06-15 18:46:08 -0300

    Implementada as interfaces da pasta de interface na camada de Domain

---
+++ src/Ecommerce.Domain/Interfaces/Persons/Customers/ICustomerRepository.cs
@@ -0,0 +1,14 @@
+﻿using Ecommerce.Domain.Models.Persons.Customers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Domain.Interfaces.Persons.Customers
+{
+    public interface ICustomerRepository : IRepository<Customer>
+    {
+        Customer GetByEmail(string email);
+    }
+}
---
+++ src/Ecommerce.Domain/Interfaces/Persons/Users/IUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Domain.Interfaces.Persons.Users
+{
+    public interface IUser
+    {
+        string Nmae { get; }
+
+        bool IsAuthenticated();
+
+        IEnumerable<Claim> GetClaimIdentity();
+    }
+}
