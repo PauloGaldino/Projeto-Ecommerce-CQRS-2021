7c928dcad3ec06b403e0648ead625163ac977544
JOSE GALDINO <jose.galdino4@fatec.sp.gov.br>
2021-06-17 14:27:20 -0300

    Implementada a classe na camada de CrossCutting.Bus

---
+++ src/Ecommerce.Infra.CrossCutting.Bus/InMemoryBus.cs
@@ -0,0 +1,38 @@
+﻿using Ecommerce.Domain.Core.Bus.Interfaces;
+using Ecommerce.Domain.Core.Commands;
+using Ecommerce.Domain.Core.Events;
+using Ecommerce.Domain.Core.Events.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Infra.CrossCutting.Bus
+{
+    public sealed class InMemoryBus : IMediatorHandler
+    {
+        private readonly IMediator _mediator;
+        private readonly IEventStore _eventStore;
+
+        public InMemoryBus(IMediator mediator, IEventStore eventStore)
+        {
+            _mediator = mediator;
+            _eventStore = eventStore
+        }
+
+        public Task RaiseEvent<T>(T @event) where T : Event
+        {
+            if (!@event.MessageType.Equals("DomainNotification"))
+                _eventStore?.Save(@event);
+
+            return _mediator.Publish(@event);
+        }
+
+        public Task SendCommand<T>(T command) where T : Command
+        {
+            return _mediator.Send(command);
+        }
+    }
+}
