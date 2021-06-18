b770bff120d6af8b565124dbc6eaa2abd61552fe
JOSE GALDINO <jose.galdino4@fatec.sp.gov.br>
2021-06-18 13:31:55 -0300

    Implementada as classe da models e de services na camada de CrossCuttings.Identity

---
+++ src/Ecommerce.Infra.CrossCotting.Identity/Extensions/EmailSenderExtension.cs
@@ -0,0 +1,20 @@
+﻿using Ecommerce.Infra.CrossCotting.Identity.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Infra.CrossCotting.Identity.Extensions
+{
+    public static class EmailSenderExtension
+    {
+        public static Task SendEmailConfirmation(this IEmailSender emailSender, string email, string link)
+        {
+            return emailSender.SendEmailAsync(email, "Confirm your email",
+            $"Please confirm your account by clicking this link: <a href='{HtmlEncoder.Default.Encode(link)}'>link</a>");
+
+        }
+    }
+}
