6fe5a0aee4460aafa47a972361f6f9bdd25d399f
JOSE GALDINO <jose.galdino4@fatec.sp.gov.br>
2021-06-18 22:27:04 -0300

    Implementada as classe de produtoViwModel e clienteViewModel

---
+++ src/Ecommerce.Application/ViewModels/Persons/Customers/CustomerViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Application.ViewModels.Persons.Customers
+{
+    public class CustomerViewModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "The Name is Required")]
+        [MinLength(2)]
+        [MaxLength(100)]
+        [DisplayName("Nome")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "The E-mail is Required")]
+        [EmailAddress]
+        [DisplayName("E-mail")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "The BirthDate is Required")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
+        [DataType(DataType.Date, ErrorMessage = "Data em formato inválido")]
+        [DisplayName("Data de aniversário")]
+        public DateTime BirthDate { get; set; }
+
+    }
+}
---
+++ src/Ecommerce.Application/ViewModels/Products/ProductViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Application.ViewModels.Products
+{
+    public class ProductViewModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "The Name is Required")]
+        [MinLength(2)]
+        [MaxLength(100)]
+        [DisplayName("Nome")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "The product value is Required")]
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        [DisplayName("preço")]
+        public string Value { get; set; }
+
+        [Display(Name = "Estado")]
+        public bool State { get; set; }
+    }
+}
