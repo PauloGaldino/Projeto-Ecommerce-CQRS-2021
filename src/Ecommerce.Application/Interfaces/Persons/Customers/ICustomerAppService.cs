07f6ec9f9c5a5ae635815a43a8c7cf2f553a060a
JOSE GALDINO <jose.galdino4@fatec.sp.gov.br>
2021-06-18 23:50:28 -0300

    Implementado as Interfaces do produto e do cliente na camada de aplicação.

---
+++ src/Ecommerce.Application/Interfaces/Persons/Customers/ICustomerAppService.cs
@@ -0,0 +1,19 @@
+﻿using Ecommerce.Application.ViewModels.Persons.Customers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Application.Interfaces.Persons.Customers
+{
+    public interface ICustomerAppService : IDisposable
+    {
+        void Register(CustomerViewModel customerViewModel);
+        IEnumerable<CustomerViewModel> GetAll();
+        CustomerViewModel GetById(Guid id);
+        void Update(CustomerViewModel customerViewModel);
+        void Remove(Guid id);
+        IList<CustomerHistoryData> GetAllHistory(Guid id);
+    }
+}
---
+++ src/Ecommerce.Application/Interfaces/Products/IProductAppService.cs
@@ -0,0 +1,19 @@
+﻿using Ecommerce.Application.ViewModels.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Application.Interfaces.Products
+{
+    public interface IProductAppService : IDisposable
+    {
+        void Register(ProductViewModel productViewModel);
+        IEnumerable<ProductViewModel> GetAll();
+        ProductViewModel GetById(Guid id);
+        void Update(ProductViewModel productViewModel);
+        void Remove(Guid id);
+        IList<ProductHistoryData> GetAllHistory(Guid id);
+    }
+}
