6dfff3eb1e0fc7f81551b4d797e72d05ad380aee
JOSE GALDINO <jose.galdino4@fatec.sp.gov.br>
2021-07-14 16:02:40 -0300

    Refatorando o código

---
+++ src/Ecommerce.Domain.Core/Enuns/ShopStatus.cs
@@ -0,0 +1,14 @@
+﻿namespace Ecommerce.Domain.Core.Enuns
+{
+    public enum ShopStatus
+    {
+        In_Cart_Product = 1,
+        Bought_Product = 2,
+        In_Preparation_Product = 3,
+        Sent_Product = 4,
+        On_The_Way_Product = 5,
+        Delivered_Product = 6,
+        Received_Product = 7,
+        Canceled_Product = 8
+    }
+}
